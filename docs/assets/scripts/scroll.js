0dabc7daac84fb06fadce4973e1595da1fa4a217
Ji-hoon Lee <anteater1056@gmail.com>
2022-11-28 22:07:21 +0900

    약간의 레이아웃 변경 및 위로 버튼 추가

---
+++ docs/assets/scripts/scroll.js
@@ -0,0 +1,22 @@
+const scrollBtn = document.querySelector(".global-btn-up");
+
+const btnVisibility = () => {
+  if (window.scrollY > 400) {
+    scrollBtn.style.visibility = "visible";
+  } else {
+    scrollBtn.style.visibility = "hidden";
+  }
+
+  console.log(window.scrollY);
+};
+
+document.addEventListener("scroll", () => {
+  btnVisibility();
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
