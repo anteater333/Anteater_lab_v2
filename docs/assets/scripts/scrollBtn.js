94cced29a8dba2b9c5c54e62be9337e887b1feed
Ji-hoon Lee <anteater1056@gmail.com>
2023-06-18 21:29:43 +0900

    브라우저 너비 절반 시 위로 스크롤 버튼 위치 추가

--- docs/assets/scripts/scrollBtn.js
+++ docs/assets/scripts/scrollBtn.js
@@ -1,22 +1,27 @@
-const scrollBtn = document.querySelector(".up-btn");
-const btnContainer = document.querySelector(".up-btn-container");
+const scrollBtnList = document.querySelectorAll(".up-btn");
+const btnContainerList = document.querySelectorAll(".up-btn-container");
 
-const btnVisibility = () => {
-  if (window.scrollY > 100) {
-    btnContainer.classList.add("show");
-  } else {
-    btnContainer.classList.remove("show");
-  }
-};
+for (let i = 0; i < scrollBtnList.length; i++) {
+  const btnContainer = btnContainerList[i];
+  const scrollBtn = scrollBtnList[i];
 
-document.addEventListener("scroll", () => {
-  btnVisibility();
-});
+  const btnVisibility = () => {
+    if (window.scrollY > 100) {
+      btnContainer.classList.add("show");
+    } else {
+      btnContainer.classList.remove("show");
+    }
+  };
 
-scrollBtn.addEventListener("click", () => {
-  history.pushState(null, null, window.location.href.split('#')[0]);
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+  document.addEventListener("scroll", () => {
+    btnVisibility();
   });
-});
+
+  scrollBtn.addEventListener("click", () => {
+    history.pushState(null, null, window.location.href.split("#")[0]);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
